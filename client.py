9651fb2f4b0796690096318aa952f2ad14341872
VieEeEw <786851669@qq.com>
2019-09-22 23:36:16 -0500

    123

--- client.py
+++ client.py
@@ -14,26 +14,35 @@ print("Connected")
 vid = cv2.VideoCapture(0)
 count = 0
 while vid.isOpened():
-    count += 1
-    retval, image = vid.read()
-    if count == 100:
-        count = 0
-        img_encode = cv2.imencode('.jpg', image)[1].tostring()
-        MESSAGE_SIZE = ("SIZE " + str(len(img_encode))).encode()
-        try:
-            s.sendall(MESSAGE_SIZE)
-            print("Sent size")
-            reply = s.recv(BUFFER_SIZE)
-            print("Got reply {}".format(reply.decode()))
-            if reply.startswith(b'GOT SIZE'):
-                s.sendall(img_encode)
+    try:
+        count += 1
+        retval, image = vid.read()
+        cv2.imshow("Camera", image)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+        if count == 10:
+            count = 0
+            img_encode = cv2.imencode('.jpg', image)[1].tostring()
+            MESSAGE_SIZE = ("SIZE " + str(len(img_encode))).encode()
+            try:
+                s.sendall(MESSAGE_SIZE)
+                print("Sent size")
                 reply = s.recv(BUFFER_SIZE)
                 print("Got reply {}".format(reply.decode()))
-            if reply.startswith(b'GOT IMAGE'):
-                s.sendall(b"NEXT IMAGE")
-                print('Image sent')        
-        except:
-            pass
-
+                if reply.startswith(b'GOT SIZE'):
+                    s.sendall(img_encode)
+                    reply = s.recv(BUFFER_SIZE)
+                    print("Got reply {}".format(reply.decode()))
+                if reply.startswith(b'GOT IMAGE'):
+                    s.sendall(b"NEXT IMAGE")
+                    print('Image sent')        
+            except Exception:
+                pass
+    except KeyboardInterrupt:
+        vid.release()
+        s.close()
+        exit(0)
+vid.release()
+s.close()
             
 print("Message sent")
